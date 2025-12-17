ef8563c2843df5660d6d1a4c7f6c874f926a88c9
Jacky Fung <j12funki@gmail.com>
2025-12-16 23:43:14 -0800

    fix: restored Tailwind directives in globals.css after installing shadcn

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,35 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Page() {
     return (
         <section>
-            WordPress developer with 6+ years of agency experience building, optimizing, and maintaining custom WordPress/WooCommerce sites. Specialize in pixel-perfect Figma-to-WordPress conversions, payment gateway integrations, performance optimization, and long-term client retainers.
-        
-        <div className="tech-section mt-[35px] font-bold">
-            Technical Skills
-        </div>
+            <p>
+                WordPress developer with 6+ years of agency experience building, optimizing, and maintaining custom WordPress/WooCommerce sites. Specialize in pixel-perfect Figma-to-WordPress conversions, payment gateway integrations, performance optimization, and long-term client retainers.
+            </p>
+            <div className="tech-section mt-[35px] font-bold">
+                Technical Skills
+            </div>
+            <div className="">
+                <h1>Wordpress</h1>
+                <ul className="flex flex-wrap gap-3">
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>Javascript</li>
+                <li>jQuery</li>
+                <li>WordPress</li>
+                <li>Woocommerce</li>
+                <li>PHP</li>
+                <li>DNS</li>
+                <li>Git</li>
+                </ul>
+            </div>
+            
+
         
         </section>
 

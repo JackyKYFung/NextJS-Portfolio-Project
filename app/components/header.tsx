fd7b6c9d3c88eca997fbb4001deb999d0703dac0
Jacky Fung <jackyfung@Jackys-MacBook-Pro.local>
2026-04-13 10:23:01 -0700

    added usePathname to add css to nav menu items when it's active

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,11 @@
+"use client";
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 export function Header() {
+    const pathname = usePathname();
+    //This "reads" the current URL state
+
     return (
         <header className="flex justify-between mb-[30px] content-center">
             <div className="font-bold text-2xl">
@@ -10,16 +15,44 @@ export function Header() {
             <nav>
                 <ul className="flex gap-5">
                     <li>
-                        <Link href={'/'}>Home</Link>
+                        <Link 
+                            href='/'
+                            className={pathname === "/" ? "font-bold" : ""}
+                            >
+                                Home
+                        </Link>
                     </li>
                     <li>
-                        <Link href={'#about'}>About</Link>
+                        <Link 
+                            href='/about'
+                            className={pathname === "/about" ? "font-bold" : ""}
+                            >
+                                About
+                        </Link>
                     </li>
                     <li>
-                        <Link href={'#projects'}>Projects</Link>
+                        <Link 
+                            href='/projects'
+                            className={pathname === "/projects" ? "font-bold" : ""}
+                            >
+                                Projects
+                        </Link>
                     </li>
                     <li>
-                        <Link href={'#contact'}>Contact</Link>
+                        <Link 
+                            href='/experience'
+                            className={pathname === "/experience" ? "font-bold" : ""}
+                            >
+                                Experience
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            href='/contact'
+                            className={pathname === "/contact" ? "font-bold" : ""}
+                            >
+                                Contact
+                        </Link>
                     </li>
                 </ul>
             </nav>
--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -7,6 +7,7 @@ export default async function ExperiencePage() {
     return (
         <section>
             <h1 className="experiencePageComp">Experience</h1>
+            {/* passes data to Experience component*/}
             <Experience experiences={data} />
         </section>
     )

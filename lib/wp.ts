cf0e7cf31712ee305ae17fa8ba63a3349d251724
Jacky Fung <jackyfung@Jackys-MacBook-Pro.local>
2026-04-29 17:35:27 -0700

    updated code and fetch call URL as well as adding no-cache to fix the API data not returning project tech stack tags

--- lib/wp.ts
+++ lib/wp.ts
@@ -1,11 +1,14 @@
 const BASE_URL = "https://jfunki.com/wp-json/wp/v2";
 
-export async function getData (endpoint: string, queryParams: string = "") {
-    const url = `${BASE_URL}/${endpoint}?_embed${queryParams}`;
+export async function getData(endpoint: string, queryParams: string = "") {
+    // Ensure queryParams starts with & if it exists
+    const formattedParams = queryParams ? `&${queryParams}` : "";
+    const url = `${BASE_URL}/${endpoint}?_embed${formattedParams}`;
 
-    const res = await fetch(url, { next: { revalidate: 3600 },
-    cache: 'no-store' } );
-    //revalidate checks Wordpress for changes every 3600 seconds
+    const res = await fetch(url, { 
+        cache: 'no-store',
+        next: { revalidate: 0 } 
+    });
 
     if (!res.ok) {
         throw new Error(`Failed to fetch data from ${endpoint}`);

7787f34a0432c55a1139304d9a605119df7fa46e
Jacky Fung <jackyfung@Jackys-MacBook-Pro.local>
2026-06-18 00:18:38 -0700

    removed unused contact page.tsx

--- app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Contact } from "@/app/components/Contact";
-
-export default function ContactPage() {
-    return (
-        <main className="animate-fade-in">
-            <Contact />
-        </main>
-    )
-}
